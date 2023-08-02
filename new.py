ad12b9925ded38f9f6639d071c903b6ee99793ec
alisher <alishbakhtiyor@gmail.com>
2023-08-02 19:40:57 +0500

    new code

---
+++ new.py
@@ -0,0 +1,8 @@
+# import random
+
+# # names = ["Bobojon", "Suman", "Siyovush", "Akai Umed", "Zimfira", "Umedjon", "Alisher"]
+# # print(random.choice(names))
+
+
+# my_random = random.randint(10, 500)
+# print(my_random)
